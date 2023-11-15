c17b9ae6f7453113e0a110a86421cd964c54918d
John Garbutt <john.garbutt@stackhpc.com>
2023-11-15 13:06:33 +0000

    Move default helm to 0.1.4 (#28)
    This includes flatcar support, updated addons and is better tested
    on Ubuntu jammy.

--- magnum_capi_helm/conf.py
+++ magnum_capi_helm/conf.py
@@ -58,7 +58,7 @@ capi_helm_opts = [
     ),
     cfg.StrOpt(
         "default_helm_chart_version",
-        default="0.1.2-dev.0.main.55",
+        default="0.1.4",
         help=(
             "Version of the helm chart specified "
             "by the config: capi_driver.helm_chart_repo "
